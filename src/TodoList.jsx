d4691bc0d31badf0da869dacc4da6bb7b5d8af16
Blanquitta <lilian0317@aol.com>
2026-05-03 00:50:15 -0400

    lesson-04-hanlers

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,27 +1,14 @@
 
-import TodoForm from "./TodoForm";
 import TodoListItem from "./TodoListItem";
 
-
-function TodoList({todoList}) {
-     
-
- const count = []
-    return (
-
-      <>
-      <ul>
-       
-        
-        {todoList.map((todo )=>(<TodoListItem key={todo.id} todo={todo} /> ))};
-      </ul>
-    </>
-  )
-     
- 
-
-
-
+function TodoList({ todoList }) {
+  return (
+    <ul>
+      {todoList.map((todo) => (
+        <TodoListItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
 }
 
 export default TodoList;
