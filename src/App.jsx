03aef3eaa24c0a069fa194fe41ee1b1133511b7a
Blanquitta <lilian0317@aol.com>
2026-05-06 19:55:42 -0400

    lesson-05-forms

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,13 @@
 // // import './App.css'
 import {useState} from 'react'; 
 import TodoForm from './TodoForm.jsx';
+import TodoList from './TodoList.jsx';
 
 
   function App() {
   const [todos, setTodos] = useState([]);
 
-  function  completeTodo(id) {
-  setTodoList((prev) =>
-    prev.map((todo) =>
-    
-    
-    todo.id === id
-      ? { ...todo, isCompleted: true }
-      : todo
-)
-);
-}
+
 function addTodo(title) {
     const newTodo = {
       id: Date.now (),
@@ -28,14 +19,26 @@ function addTodo(title) {
     setTodos(previous => [newTodo, ...previous]);
    
   }
+function  completeTodo(id) {
+  const updatedTodos = todos.map((todo)=>
+   todo.id === id
+      ? { ...todo, isCompleted: true }
+      : todo
+  )
+      setTodos(updatedTodos)
 
+}
  return (
-    <div>
+    <>
       <h1>My Todos</h1>
 
       <TodoForm onAddTodo={addTodo} />
+      <TodoList
+      TodoList={TodoList}
+      oncompleteTodo={completeTodo}
       
-    </div>
+    />
+    </>
   );
 } 
   
--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -11,15 +11,14 @@ function TodoForm({ onAddTodo }) {
 
   const handleAddTodo = (event) => {
     event.preventDefault();
-    // onAddTodo(inputRef.current.value);
-     const handleAddTodo = (event) => {
-    event.preventDefault();
+    
+    
     
     onAddTodo(workingTodoTitle);
     setWorkingTodoTitle("");
   };
 
- };
+ 
 
   return ( 
   <form onSubmit={handleAddTodo}>
