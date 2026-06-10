fbbc2a335af5c931ee8cfbc255cbbfbf8373fa27
Blanquitta <lilian0317@aol.com>
2026-06-10 18:36:56 -0400

    lesson-04-handlers

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,25 +1,29 @@
-
-
-
-import {useRef} from 'react';
-
+import { useRef } from "react";
 
 function TodoForm({ onAddTodo }) {
   const inputRef = useRef();
 
   const handleAddTodo = (event) => {
     event.preventDefault();
-    onAddTodo(inputRef.current.value);
+    // onAddTodo(inputRef.current.value.);
+    const todoTitle = inputRef.current.value.trim();
+    if (!todoTitle) {
+      return;
+    }
+
+    onAddTodo(todoTitle);
+
+    inputRef.current.value = "";
   };
 
   return (
-  <form onSubmit={handleAddTodo}>
-    <label htmlFor ="todoTitle"> Todo </label>
-    <input type='text'id='todotitle' ref={inputRef}/>
-    
-    <button type="submit" > Add Todo </button>
-  </form>
-);
+    <form onSubmit={handleAddTodo}>
+      <label htmlFor="todoTitle"> Todo </label>
+      <input type="text" id="todotitle" ref={inputRef} />
+
+      <button type="submit"> Add Todo </button>
+    </form>
+  );
 }
 
 export default TodoForm;
