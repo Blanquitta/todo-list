5763374e610e555b4a3911a88a3260ec07521ef7
Blanquitta <lilian0317@aol.com>
2026-06-12 19:20:26 -0400

    lesson-07-datafetching

--- src/features/TodosPage.jsx
+++ src/features/TodosPage.jsx
@@ -1,35 +1,31 @@
-import { useState } from "react";
-import TodoForm from "src/features/TodoForm.jsx";
-import TodoList from "src/features/TodoList/TodoList.jsx";
+// import TodoForm from "src/features/TodoForm.jsx";
+// import TodoList from "src/features/TodoList/TodoList.jsx";
+import { useState, useEffect } from "react";
 
 export default function TodosPage({ token }) {
+  // keep state
   const [todoList, setTodoList] = useState([]);
   const [error, setError] = useState("");
   const [isTodoListLoading, setIsTodoListLoading] = useState(false);
 
-  function updateTodo(editedTodo) {
+  function addTodo(title) {
+    const newTodo = {
+      id: Date.now(),
+      title,
+      isCompleted: false,
+    };
+
+    setTodoList((previous) => [newTodo, ...previous]);
+  }
+
+  function completeTodo(id) {
     const updatedTodos = todoList.map((todo) =>
-      todo.id === editedTodo.id ? { ...editedTodo } : todo,
+      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo,
     );
 
     setTodoList(updatedTodos);
   }
 
-  function addTodo(title) {
-    const newTodo = {
-      id: Date.now(),
-
-      title,
-      isCompleted: false,
-    };
-
-    setTodos((previous) => [newTodo, ...previous]);
-  }
-  function completeTodo(id) {
-    const updatedTodos = todos.map((todo) =>
-      todo.id === id ? { ...todo, isCompleted: true } : todo,
-    );
-  }
   useEffect(() => {
     async function fetchTodos() {
       setIsTodoListLoading(true);
@@ -53,7 +49,6 @@ export default function TodosPage({ token }) {
         }
 
         const data = await response.json();
-
         setTodoList(data.tasks);
       } catch (error) {
         setError(error.message);
@@ -68,9 +63,16 @@ export default function TodosPage({ token }) {
   return (
     <>
       <h1>My Todos</h1>
-      <TodoForm onAddTodo={addTodo} />
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {isTodoListLoading ? (
+        <p>Loading tasks...</p>
+      ) : (
+        <>
+          <TodoForm onAddTodo={addTodo} />
 
-      <TodoList todoList={todos} onCompleteTodo={completeTodo} />
+          <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
+        </>
+      )}
     </>
   );
 }
